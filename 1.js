0c44e3533dacfd9a567254cc122f24992828d1c0
YuqingDuan <yduan@laurentian.ca>
2018-10-18 11:50:31 -0400

    1150

---
+++ 1.js
@@ -0,0 +1,17 @@
+// alert()  在页面弹出一个对话框，早期JS调试使用。
+// alert("alert");
+
+// confirm()  在页面弹出一个对话框, 常配合if判断使用。
+// confirm("confirm");
+
+// console.log()  将信息输入到控制台，用于js调试。
+// console.log("console.log");
+
+// prompt() 弹出对话框，用于接收用户输入的信息。
+// prompt("prompt");
+
+// document.write()在页面输出消息
+// document.write("document.write");
+
+//转义\
+alert("\"");
